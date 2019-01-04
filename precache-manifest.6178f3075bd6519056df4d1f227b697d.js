dc2de52a6d8c70b4c0669968f2793b4b5d906c15
Faysal AlZoubi <faysal.zoabi@gmail.com>
2019-01-04 09:36:03 +0100

    Updates

---
+++ precache-manifest.6178f3075bd6519056df4d1f227b697d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f4fe3edfe7646282e88",
+    "url": "/Recipe_App/static/js/runtime~main.1f4fe3ed.js"
+  },
+  {
+    "revision": "a9b0219a10235631093a",
+    "url": "/Recipe_App/static/js/main.a9b0219a.chunk.js"
+  },
+  {
+    "revision": "f2a03e1ac6ab04610fdb",
+    "url": "/Recipe_App/static/js/1.f2a03e1a.chunk.js"
+  },
+  {
+    "revision": "a9b0219a10235631093a",
+    "url": "/Recipe_App/static/css/main.e1e43067.chunk.css"
+  },
+  {
+    "revision": "57ad8a4472322b0fa815bc796b88ebea",
+    "url": "/Recipe_App/index.html"
+  }
+];
